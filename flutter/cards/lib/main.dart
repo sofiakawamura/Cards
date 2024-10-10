2bd521fa6971095a12a4a1e91fc65c119227c61c
sofia <cc23157@g.unicamp.br>
2024-10-10 10:41:22 -0300

    FUncionando

--- flutter/cards/lib/main.dart
+++ flutter/cards/lib/main.dart
@@ -1,8 +1,18 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:cards/screen/cards.dart';
 
 void main() {
   runApp(const MainApp());
+}
+
+class AppScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+    PointerDeviceKind.touch,
+    PointerDeviceKind.mouse
+  };
 }
 
 class MainApp extends StatelessWidget {
@@ -11,6 +21,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      scrollBehavior: AppScrollBehavior(),
       debugShowCheckedModeBanner: false,
       home: Cards()
     );
